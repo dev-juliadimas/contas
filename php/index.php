00b42eb7a97ab36513f664adba352ad35a547040
Júlia Dimas <90213543+juliadimas@users.noreply.github.com>
2025-08-13 16:19:24 -0300

    Site de Vendas

---
+++ php/index.php
@@ -0,0 +1,5 @@
+<?php
+    include_once("../bd/conexao.php");
+    include_once("../php/menu.php");
+    include_once("../php/head.php");
+?>
---
+++ php/menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul class="menu" style="display:flex; justify-content: center; align-items: center; ">
+        <li style="font-family: 'Cinzel', sans-serif;"><a href="index.php">Início</a></li> 
+        <li style="font-family: 'Cinzel', sans-serif;"><a href="imoveis.php">Imóveis</a></li> 
+        <li style="font-family: 'Cinzel', sans-serif;"><a href="sobrenos.php">Sobre Nós</a></li> 
+        <li style="font-family: 'Cinzel', sans-serif;"><a href="contato.php">Contato</a></li> 
+    </ul>
+</nav>
